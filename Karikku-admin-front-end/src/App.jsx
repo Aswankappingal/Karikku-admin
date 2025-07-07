df97d1aaf59428f003cab6345cd4f0f4235e3c28
Aswankappingal <aswankappingal@gmail.com>
2025-07-07 12:37:13 +0530

    file added

---
+++ Karikku-admin-front-end/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './Component/HomePage/HomePage'
+
+function App() {
+
+
+  return (
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
